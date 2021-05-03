b0d284987d0a3573fe386606360c635554bfc8bc
milos381 <milos_popovic@outlook.com>
2021-05-03 11:38:06 -0400

    commit tree c9f3f64f6a673714395d269e5d0cc63fb691ad64

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,27 +1,26 @@
-import uuid from 'uuid';
+
 export default (state = [], action) => {
   let indexInQuestion;
   let quoteInQuestion;
   switch (action.type) {
     case 'ADD_QUOTE':
       const quote = {
-        id: uuid(),
+        id: action.quote.id,
         content: action.quote.content,
         author: action.quote.author,
-        votes:0
+        votes: action.quote.votes
       }
       
       return state.concat(quote)
     case 'REMOVE_QUOTE':
-      return state.filter(quote => quote.id !== action.id)
+      return state.filter(quote => quote.id !== action.quoteId)
     case 'UPVOTE_QUOTE':
-      
-      indexInQuestion = state.findIndex(quote => quote.id === action.id)
+      indexInQuestion = state.findIndex(quote => quote.id === action.quoteId)
       quoteInQuestion = state[indexInQuestion]
       return [...state.slice(0, indexInQuestion), Object.assign({},quoteInQuestion, { votes: quoteInQuestion.votes += 1 } ),...state.slice(indexInQuestion + 1)]
     case 'DOWNVOTE_QUOTE':
       
-      indexInQuestion = state.findIndex(quote => quote.id === action.id)
+      indexInQuestion = state.findIndex(quote => quote.id === action.quoteId)
       quoteInQuestion = state[indexInQuestion]
       if (quoteInQuestion.votes > 0) {
         return [...state.slice(0, indexInQuestion), Object.assign({},quoteInQuestion, { votes: quoteInQuestion.votes -= 1 } ),...state.slice(indexInQuestion + 1)]
